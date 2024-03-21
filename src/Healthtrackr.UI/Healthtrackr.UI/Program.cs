11da7aa4893a68336264e3a6eefb4c8b255e9160
Will Velida <willvelida@hotmail.co.uk>
2024-03-21 20:40:55 +1100

    Moving to Blazor Server App
    Signed-off-by: Will Velida <willvelida@hotmail.co.uk>

--- src/Healthtrackr.UI/Healthtrackr.UI/Program.cs
+++ src/Healthtrackr.UI/Healthtrackr.UI/Program.cs
@@ -1,23 +1,28 @@
-using Healthtrackr.UI.Components;
+using Healthtrackr.UI.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
 }
 
-app.UseStaticFiles();
-app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
