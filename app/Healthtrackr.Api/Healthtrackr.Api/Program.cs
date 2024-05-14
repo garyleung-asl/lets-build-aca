9972aff7a2caa0fcf95e53bf630f86b191a29caa
Will Velida <willvelida@hotmail.co.uk>
2024-05-14 20:07:42 +1000

    Removing open telemetry for now

--- app/Healthtrackr.Api/Healthtrackr.Api/Program.cs
+++ app/Healthtrackr.Api/Healthtrackr.Api/Program.cs
@@ -1,20 +1,9 @@
-using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Healthtrackr.Api.Extensions;
 using Healthtrackr.Api.Services;
-using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Logging.AddOpenTelemetry();
-builder.Services.AddOpenTelemetry()
-    .UseAzureMonitor()
-    .WithTracing(tracing =>
-    {
-        tracing.AddAspNetCoreInstrumentation()
-               .AddHttpClientInstrumentation();
-    });
-
 builder.Services.AddSingleton<IWeightManager, FakeWeightManager>();
 
 var app = builder.Build();
